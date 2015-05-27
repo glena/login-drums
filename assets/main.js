d5674fc02ebbf75a45ccb654cb3a4ed411c15c06
German Lena <german.lena@gmail.com>
2015-05-27 00:14:51 -0300

    a little cutier

--- assets/main.js
+++ assets/main.js
@@ -7,14 +7,31 @@ var size = {
 	axisWidth:40
 }
 
-var height = size.width - size.padding * 2;
+var height = size.width - size.padding * 2 - size.axisWidth;
 var width = size.height - size.padding * 2 - size.axisWidth;
 var svg = d3.select('svg');
+var texturesArr = [];
+
+function getTexture(i) {
+
+	if (texturesArr.length > i) return texturesArr[i];
+
+	var texture = textures.lines()
+	    .orientation("horizontal")
+	    .size(5)
+		.strokeWidth(3)
+	    .stroke(colors(i));
+
+	svg.call(texture);
+
+	texturesArr[i] = texture.url();
+	return texturesArr[i];
+}
 
 svg.attr("width", size.width)
    .attr("height", size.height);
 
-var bars = svg.append('g').attr("transform", "translate(" + [ size.padding + size.axisWidth,size.padding ] + ")");
+var bars = svg.append('g').attr("transform", "translate(" + [ size.padding + size.axisWidth,size.padding + size.axisWidth] + ")");
 
 var x = d3.scale.ordinal().rangeBands([0, width], 0.1);
 var y = d3.scale.linear().range([height, 0]);
@@ -26,7 +43,7 @@ var yAxis = d3.svg.axis()
 
 var yAxisEl = svg.append("g")
 		.attr("class", "y axis")
-		.attr("transform", "translate(" + [ size.axisWidth,size.padding ] + ")")
+		.attr("transform", "translate(" + [ size.axisWidth, size.padding + size.axisWidth ] + ")")
 		.call(yAxis);
 
 var pusher = new Pusher('54da1f9bddbf14929983');
@@ -42,11 +59,11 @@ channel.bind('login', function(data) {
 function addData(d) {
 
 	var item;
-	var index = _.findIndex(data, function(e) {return e.name == d.conn;});
+	var index = _.findIndex(data, function(e) {return e.name == d.strategy;});
 
 	if (index == -1) {
 		item = {
-			name: d.conn,
+			name: d.strategy,
 			stamps: []
 		}
 		data.push(item);
@@ -88,13 +105,25 @@ function updateData() {
 			    .attr("x", function(d, i) { return x(i); }) 
 				.attr("y", height)
 				.attr("width", x.rangeBand())
-				.style("fill", function(d,i){ return colors(i); });
+				.style("fill", function(d,i){ return getTexture(i); });
 
 	rects.exit().remove();
 
 	rects.transition()
 				.attr("y", function(d) { return y(d.value); })
 				.attr("height", function(d) { return height-y(d.value); })
-				.attr("width", width * 0.8 / data.length)
+				.attr("width", x.rangeBand())
 				.attr("x", function(d, i) { return x(i); });
+
+	var texts = bars.selectAll('text.xaxis').data(data);
+	texts.enter()
+			.append('text')
+				.classed('xaxis',true)
+			    .attr("x", function(d, i) { return x(i) + (x.rangeBand() / 2); }) 
+				.attr("y", height + size.padding)
+				.text(function(d){return d.name;});
+
+	texts.exit().remove();
+
+	texts.transition().attr("x", function(d, i) { return x(i) + (x.rangeBand()/2); });
 }
